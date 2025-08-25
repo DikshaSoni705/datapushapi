2f9209521c4bd7a287e8cc8c0f3231cb7fcfd41b
DikshaSoni705 <sonidiksha1602@gmail.com>
2025-08-25 14:53:31 +0530

    wertyu

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost:27017/test';
+const mongoURL = 'mongodb+srv://diksha089:Soni%401616@cluster0.ns6hza9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
   try {
@@ -10,6 +10,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
+
     process.exit(1); // Exit process with failure
   }
 };
